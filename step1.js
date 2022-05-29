050dc1f10a1ab77928d060c3d7dc32f2531b4109
Your Name <you@example.com>
2022-05-29 12:35:40 -0400

    explanation added

--- step1.js
+++ step1.js
@@ -1,3 +1,9 @@
+//In step1.js, write a function, cat.It should take one argument, path, and it should read 
+//the file with that path, and print the contents of that file.Then, write some code that 
+//calls that function, allowing you to specify the path argument via the command line.
+//If you give it the path of a non-existent file, it should print that error and halt the script execution:
+
+
 const fs = require('fs');
 //fs module provides useful functionality to access and interact with the file
 //system. as fs.read, fs.rename, fs.writeFile
--- step2.js
+++ step2.js
@@ -1,3 +1,8 @@
+//Copy over your step1.js code to step2.jsAdd a new function, webCat. This should take a URL
+//and, using axios, should read the content of that URL and print it to the console.Modify the
+//code that invoked cat so that, based on the command-line args, it decides whether the 
+//argument is a file path or a URL and calls either cat or webCat, respectively.
+//If there is an error getting the page, it should print that.
 const fs = require('fs');
 const process = require('process');
 const axios = require('axios');
